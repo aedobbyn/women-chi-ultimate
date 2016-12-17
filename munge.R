657dcb5cdbe9610aa01e7020b0184cb8b05ec611
aedobbyn <adobbs11@gmail.com>
2016-12-17 12:13:16 -0600

    split charts into their own file

--- munge.R
+++ munge.R
@@ -64,27 +64,7 @@ contrasts(demographics$age) <- c(1:6)
 
 
 
-# make histogram of age distribution
-age_hist <- ggplot(aes(age), data = demographics) +
-  geom_bar()
-age_hist
-
-
-# make histogram of team distribution
-team_hist <- ggplot(aes(team), data = demographics) +
-  geom_bar()
-team_hist
-
-# team demographics (need a better way to represent this)
-age_play_time <- ggplot(aes(team, age), data = demographics, na.rm = TRUE, stat=count) +
-  geom_point() # aes(colour = where_live, size = count)
-age_play_time
-
-
-
-
 # ---------------------------------
-
 
 # make development and inclusion tbl
 dev_inclusion <- sorted[, 34:37]
@@ -106,8 +86,6 @@ dev_inclusion$mixed <- factor(dev_inclusion$mixed)
 
 
 
-
-
 # relevel 
 new_levels <- c("Agree", "Somewhat Agree", "Neutral - I don't have an opinion here.",
                 "Somewhat disagree", "Disagree")
@@ -119,29 +97,6 @@ levels(dev_inclusion$mixed) <- new_levels
 
 # check levels
 levels(dev_inclusion$UC)
-
-
-dev_incl_plot <- ggplot(aes(women), data = na.omit(dev_inclusion)) +
-  geom_bar()
-dev_incl_plot
-
-dev_incl_plot <- ggplot(aes(mixed), data = na.omit(dev_inclusion)) +
-  geom_bar()
-dev_incl_plot
-
-dev_incl_plot <- ggplot(aes(UC), data = na.omit(dev_inclusion)) +
-  geom_bar()
-dev_incl_plot
-
-dev_incl_plot <- ggplot(aes(college), data = na.omit(dev_inclusion)) +
-  geom_bar()
-dev_incl_plot
-
-dev_incl_plot <- ggplot(aes(college), data = dev_inclusion, na.rm=TRUE) +
-  geom_bar(fill = "red", position="dodge") +
-  geom_bar(aes(women), fill = "blue", position="dodge")
-dev_incl_plot
-
 
 
 
@@ -161,12 +116,12 @@ mixed_teams <- c("ELevate", "Jabba The Huck",
 demogr_inclus$team_type <- 
   ifelse(demogr_inclus$team %in% mixed_teams, demogr_inclus$team_type <- "mixed",
          ifelse(demogr_inclus$team %in% womens_teams, demogr_inclus$team_type <- "womens",
-                demogr_inclus$team_type <- "no_team"))
+                demogr_inclus$team_type <- "other"))
 
 # from team_type, make club_or_not column
 demogr_inclus$club_or_not <- 
-  ifelse(demogr_inclus$team_type == "no_team",
-         demogr_inclus$club_or_not <- "no_club",
+  ifelse(demogr_inclus$team_type == "other",
+         demogr_inclus$club_or_not <- "not_club",
          demogr_inclus$club_or_not <- "club")
 
 # make these new variables factors
@@ -182,6 +137,14 @@ m.age <- glm(club_or_not ~ age,
 
 m.UC.team_type <- aov(UC ~ team_type,
          data = na.omit(demogr_inclus))
+
+
+
+
+
+
+
+
 
 
 
